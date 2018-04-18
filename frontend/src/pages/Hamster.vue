69c5c509be4b8d9ceabd5f26d92f77f6228da50f
gujunmin <junmin.gu@ele.me>
2018-04-18 10:20:10 +0800

    feat: 前端打地鼠页面部分完成

--- frontend/src/pages/Hamster.vue
+++ frontend/src/pages/Hamster.vue
@@ -2,8 +2,8 @@
   <div class="hamster">
     <div class="header">
       <div class="btn-container">
-        <el-button type="primary">开始游戏</el-button>
-        <el-button>结束游戏</el-button>
+        <el-button type="primary" @click="startGame">开始游戏</el-button>
+        <el-button @click="endGame">结束游戏</el-button>
       </div>
       <div class="game-infos">
         <span>分数: {{ score }}分</span>
@@ -21,8 +21,13 @@
             v-for="j in [1, 2, 3]"
             :key="j"
             @click="hitHamsterHandler([i, j])"
-            class="cell"
-            v-if="cellHavingHamster.includes(`${i}${j}`)"></td>
+            class="cell">
+            <img
+              class="hamster"
+              v-if="cellHavingHamster.includes(`${i}${j}`)" 
+              src="../assets/hamster.jpeg"
+              alt="">
+          </td>
         </tr>
       </tbody>
     </table>
@@ -35,33 +40,68 @@ export default {
     return {
       score: 0,
       hitRate: 0,
-      timeLeft_secs: 0,
+      timeLeft_secs: 60,
       cellHavingHamster: [],
+
+      timeLeftInterval: null,
+      releaseInterval: null,
     }
   },
   methods: {
     hitHamsterHandler(coordinate) {
-      const [i, j] = coordinate
+      this.score += 1
+      this.hitRate = ((this.score / 60) * 100).toFixed(2)
+
+      const index = this.cellHavingHamster.indexOf(coordinate.join(''))
+
+      if (index !== -1) {
+        this.cellHavingHamster.splice(index, 1)
+      }
     },
 
     calculateTimeLeft() {
+      this.timeLeftInterval = setInterval(() => {
+        this.timeLeft_secs -= 1
 
+        if (this.timeLeft_secs == 0) {
+          return endGame()
+        }
+      }, 1000)
     },
 
     releaseHamster() {
-      
+      const tempI = Math.floor(Math.random() * 3 + 1)
+      const tempJ = Math.floor(Math.random() * 3 + 1)
+      const hamsterCoordinate = `${tempI}${tempJ}`
+
+      this.cellHavingHamster.push(hamsterCoordinate)
+
+      setTimeout(() => {
+        const index = this.cellHavingHamster.indexOf(hamsterCoordinate)
+
+        if (index !== -1) {
+          this.cellHavingHamster.splice(index, 1)
+        }
+      }, 3000)
     },
 
     startGame() {
-
+      this.releaseInterval = setInterval(this.releaseHamster, 1000)
+      this.calculateTimeLeft()
     },
 
     endGame() {
+      clearInterval(this.timeLeftInterval)
+      clearInterval(this.releaseInterval)
+      this.score = 0
+      this.hitRate = 0
+      this.timeLeft_secs =  60
+      this.cellHavingHamster = []
 
+      // stop 弹出分数提示，并可以进入英雄榜单
     },
   },
   mounted() {
-
   },
 }
 </script>
@@ -99,6 +139,15 @@ export default {
 
   .cell {
     background-color: rgba(64, 158, 255, 0.11);
+    width: 30%;
+    height: 30%;
+    text-align: center;
+    vertical-align: center;
+
+    .hamster {
+      width: 100px;
+      height: 100px;
+    }
   }
 }
 </style>
